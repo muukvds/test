d548e31933a5e181cf1719776617c6634feda67e
muukvds <muukvds@users.noreply.github.com>
2017-11-09 22:04:58 +0100

    first commit

---
+++ src/test/java/Main.java
@@ -0,0 +1,14 @@
+package test.java;
+
+public class Main {
+
+    /**
+     * The application main starts application.
+     *
+     * @param args String[]
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+}
